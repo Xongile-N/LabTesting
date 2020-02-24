b5aa7d0a4fc0e5a6e1d1830ae04f5a1fa5ba5ae0
Xongile-OC Lab PC <xnghatsane@gmail.com>
2020-02-24 15:58:05 +0200

    Starting from scratch

--- Exercise 4.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-const FToC = function (fDeg) {
-  return 5 / 9 * (fDeg - 32)
-}
-const fOrig = -40
-console.log(`${fOrig} degrees Fahrenheit is ${FToC(fOrig)} degrees celsius`)
--- mod.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-console.log("I'm inside a module")
-module.exports = {
-  some: 'module',
-  number: 2
-}

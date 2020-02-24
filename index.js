da48b4defb460d1e4a20d29505e147ad4b05026f
Xongile-OC Lab PC <xnghatsane@gmail.com>
2020-02-24 15:22:05 +0200

    Testing something

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 'use strict'
-const path = require('path')
 const express = require('express')
 const app = express()
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-app.listen(3000)
-console.log('Express server running on port 3000')
+const mainRouter = require('./mainRoutes')
+const port = process.env.port || 3000
+app.use(mainRouter)
+app.listen(port)
+console.log(`Express server running on port ${port}`)
